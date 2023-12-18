2cbf790a092f3421ce3a2f4cf5578e27f0d31b62
Andrii9991 <117731577+Andrii9991@users.noreply.github.com>
2023-12-18 20:14:42 +0200

    fix

--- src/components/BaseRange.vue
+++ src/components/BaseRange.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="base-range">
     <vue-slider
+      class="base-range__slider"
       :value="value"
       :min="min"
       :max="max"
       :enableCross="false"
       :interval="10"
-      width="200"
-      height="4"
+      :railStyle="12"
+      :minRange="100"
+      :width="200"
+      :height="4"
+      dotSize="16"
       @change="onChange"
     >
     </vue-slider>
+
+    <div class="base-range__value">
+      <p class="min-value">Min: {{ value[0] }}$</p>
+      <p class="min-value">Max: {{ value[1] }}$</p>
+    </div>
   </div>
 </template>
 
@@ -39,9 +48,51 @@ export default class BaseRange extends Vue {
 }
 </script>
 
-<style scoped lang="scss">
+<style scope lang="scss">
 .base-range {
   width: 200px;
-  background-color: $black;
+  .vue-slider-rail {
+    border-radius: 15px;
+    background-color: $grey;
+  }
+
+  .vue-slider-process {
+    background-color: $white;
+    border-radius: 15px;
+  }
+
+  .vue-slider-dot-handle {
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: $white;
+    box-sizing: border-box;
+    box-shadow: 0.5px 0.5px 2px 1px rgba(0, 0, 0, 0.32);
+  }
+  .vue-slider-dot-handle-focus {
+    box-shadow: 0px 0px 1px 2px $black;
+  }
+
+  .vue-slider-dot-tooltip-inner {
+    font-size: 14px;
+    white-space: nowrap;
+    padding: 2px 5px;
+    min-width: 20px;
+    text-align: center;
+    color: $white;
+    border-radius: 4px;
+    background-color: $grey-lite;
+    box-sizing: content-box;
+  }
+
+  &__value {
+    display: flex;
+    justify-content: center;
+
+    .min-value {
+      margin-right: 20px;
+    }
+  }
 }
 </style>
