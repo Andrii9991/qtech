96599b3df159da1b6eacedaaa2c12139253a5b7c
Andrii9991 <andriymyshchyshyn6@gmail.com>
2024-12-24 14:05:41 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import ToastPlugin from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-bootstrap.css";
+
 Vue.config.productionTip = false;
+
+Vue.use(ToastPlugin);
 
 new Vue({
   router,
